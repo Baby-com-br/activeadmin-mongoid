3b01bb1d52fcf70b8374a1029a7f64cefb213f7a
Daniel Konishi <dkonishi@gmail.com>
2013-01-24 11:16:11 -0200

    We don't need to ask for @query if it responds to a method...

--- lib/active_admin/mongoid/adaptor.rb
+++ lib/active_admin/mongoid/adaptor.rb
@@ -16,8 +16,7 @@ module ActiveAdmin
         end
 
         def respond_to?(method_id)
-          return true if @base.send(:respond_to?, method_id)
-          @query.send(:respond_to?, method_id)
+          @base.send(:respond_to?, method_id)
         end
 
         def method_missing(method_id, *args, &block)
