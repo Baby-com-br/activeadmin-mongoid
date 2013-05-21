8fb4b5b204c6334ba60bd2b923c2fa7d0ee26b80
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2013-05-21 19:11:06 -0300

    ignore case

--- spec/mongoid/adaptor_spec.rb
+++ spec/mongoid/adaptor_spec.rb
@@ -15,15 +15,20 @@ describe ActiveAdmin::Mongoid::Adaptor::Search do
   let(:search_params) { {} }
   subject(:search) { described_class.new(Product, search_params).to_a }
 
-  let!(:product) { Product.create(:name => "something totally random") }
+  let!(:product) { Product.create(:name => "Something totally random") }
   let!(:product_2) { Product.create(:name => "abra cadabra") }
 
   it { should == [product, product_2] }
 
   context "when there are search params with contains" do
-    let(:search_params) { { 'name_contains' => "something" } }
+    let(:search_params) { { 'name_contains' => "Something" } }
 
     it { should == [product] }
+
+    context "and scrambled case" do
+      let(:search_params) { { 'name_contains' => "SomeThing ToTaLLy" } }
+      it { should == [product] }
+    end
   end
 
   context "when there are search params with eq" do
